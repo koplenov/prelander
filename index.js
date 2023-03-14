53991a7b90a41c18f127f00f9aef4ea63f037461
koplenov <koplenov@gmail.com>
2023-03-14 13:59:14 +0300

    added fallback page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const api = 'https://sync.hyoo.ru/land'
+const default_page = '=iy8wtn_tky6pc'
 const query = '=(title_text;release_ref(release_blob);pages_ref=(title_text);book_ref=(title_text))'
 const port = process.env.PORT || 3000
 
@@ -7,13 +8,17 @@ const http = require( 'http' )
 const url = require( 'url' )
 
 http.createServer( async function( request, responce ) {
-	const { _escaped_fragment_ } = url.parse( request.url, true ).query
+	let { _escaped_fragment_ } = url.parse( request.url, true ).query
 
 	console.log(request.url, request.headers)
 
-	if( !_escaped_fragment_ ) {
+	if( _escaped_fragment_ === undefined ) {
 		responce.end()
 		return
+	}
+
+	if( _escaped_fragment_ === "" ) {
+		_escaped_fragment_ = default_page
 	}
 
 	const proxy_request = await fetch(
